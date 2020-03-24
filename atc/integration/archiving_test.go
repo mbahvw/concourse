0711e0935f1aced41b69839c9424432d36c3d0c0
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-03-24 11:10:37 -0400

    atc: behaviour: archiving a pipeline pauses it
    concourse/concourse#5318
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- atc/integration/archiving_test.go
+++ atc/integration/archiving_test.go
@@ -40,8 +40,6 @@ var _ = Describe("ATC Integration Test", func() {
 		}, 20*time.Second).ShouldNot(HaveOccurred())
 
 		client = login(atcURL, "test", "test")
-		givenAPipeline(client, "pipeline")
-		whenIArchiveIt(client, "pipeline")
 	})
 
 	AfterEach(func() {
@@ -50,22 +48,44 @@ var _ = Describe("ATC Integration Test", func() {
 	})
 
 	It("can archive pipelines", func() {
+		givenAPipeline(client, "pipeline")
+
+		whenIArchiveIt(client, "pipeline")
+
 		pipeline := getPipeline(client, "pipeline")
 		Expect(pipeline.Archived).To(BeTrue(), "pipeline was not archived")
 		Expect(pipeline.Paused).To(BeTrue(), "pipeline was not paused")
 	})
 
 	It("fails when unpausing an archived pipeline", func() {
+		givenAPipeline(client, "pipeline")
+		whenIArchiveIt(client, "pipeline")
+
 		_, err := client.Team("main").UnpausePipeline("pipeline")
+
 		Expect(err).To(HaveOccurred())
 		Expect(err.Error()).To(ContainSubstring("archived pipelines cannot be unpaused"))
 	})
 
 	It("archived pipelines can have their name re-used", func() {
+		givenAPipeline(client, "pipeline")
+		whenIArchiveIt(client, "pipeline")
+
 		_, version, _, _ := client.Team("main").PipelineConfig("pipeline")
 		client.Team("main").CreateOrUpdatePipelineConfig("pipeline", version, basicPipelineConfig, false)
+
 		pipeline := getPipeline(client, "pipeline")
 		Expect(pipeline.Archived).To(BeFalse(), "pipeline is still archived")
+		Expect(pipeline.Paused).To(BeTrue(), "pipeline was not paused")
+	})
+
+	It("archiving a pipeline results in it being paused", func() {
+		givenAPipeline(client, "pipeline")
+		whenIUnpauseIt(client, "pipeline")
+
+		whenIArchiveIt(client, "pipeline")
+
+		pipeline := getPipeline(client, "pipeline")
 		Expect(pipeline.Paused).To(BeTrue(), "pipeline was not paused")
 	})
 })
@@ -73,6 +93,11 @@ var _ = Describe("ATC Integration Test", func() {
 func givenAPipeline(client concourse.Client, pipelineName string) {
 	_, _, _, err := client.Team("main").CreateOrUpdatePipelineConfig(pipelineName, "0", basicPipelineConfig, false)
 	Expect(err).NotTo(HaveOccurred())
+}
+
+func whenIUnpauseIt(client concourse.Client, pipelineName string) {
+	_, err := client.Team("main").UnpausePipeline(pipelineName)
+	Expect(err).ToNot(HaveOccurred())
 }
 
 func whenIArchiveIt(client concourse.Client, pipelineName string) {
